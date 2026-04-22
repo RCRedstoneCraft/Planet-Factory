73d1c78049da6b4dcdca2d33a7cc067b57996147
RCRedstoneCraft <136265025+RCRedstoneCraft@users.noreply.github.com>
2026-04-22 12:57:01 +0200

    rnd

--- scripts/main.js
+++ scripts/main.js
@@ -10,8 +10,8 @@ function init() {
     camera = {
         starId: 0,
         planetId: 1,
-        zoom: 10,   // pixel per tile
-        x: 60,
+        zoom: 1,   // pixel per tile
+        x: 0,
         y: 0
     }
     canvas = document.getElementById("2dcanvas")
